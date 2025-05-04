42020991d914b4a267be5f163f134d8b0555ec07
Zorutiss <vicgalsan1@gmail.com>
2025-05-05 00:49:36 +0200

    Cambios en enemy y player

--- losVengadores/src/disSoftware/proyectoFinal/Enemy.java
+++ losVengadores/src/disSoftware/proyectoFinal/Enemy.java
@@ -1,5 +1,6 @@
 package disSoftware.proyectoFinal;
 
+
 public abstract class Enemy extends Character{
 	
     protected Strategy strategy;
@@ -12,7 +13,42 @@ public abstract class Enemy extends Character{
         this.strategy = strategy;
     }
     
+
     public void performAction(Character opponent) {
-    	
+        Integer enemyAction = (int)(Math.random() * 2);
+        
+        Integer damage1=0;
+        Integer damage2=0;
+
+        DamageCalculator calculator = DamageCalculator.getInstance();
+        
+		//Comprobamos el estado
+		if (state != null) {
+            state.handle(this);
+
+            // Si está paralizado, se podría interrumpir la acción:
+            if (state instanceof ParalyzedState) {
+                System.out.println(name + " está paralizado y no puede actuar.");
+                return;
+            }else{
+				switch(enemyAction) {
+				case 1:
+                    decideNextStrategy(opponent);
+                    strategy.execute(this, opponent);
+				break;
+				case 2:
+					damage1=calculator.calculateDamage(this,opponent);
+					if(damage1==0) {
+						System.out.println(this.getName()+" se protege ante el ataque");
+					}else {
+						this.setHealth(opponent.health-damage1);
+						System.out.println(this.getName()+ "ataca y genera "+ damage1+ "de daño sobre "+ opponent.getName());
+					}
+				break;
+				}
+			}
+        }  
     }
+    protected abstract void decideNextStrategy(Character opponent);
+
 }
--- losVengadores/src/disSoftware/proyectoFinal/Player.java
+++ losVengadores/src/disSoftware/proyectoFinal/Player.java
@@ -22,39 +22,38 @@ public class Player extends Character{
     	DamageCalculator calculator = DamageCalculator.getInstance();
 		playerAction=scanner.nextInt();
       
-    	switch(playerAction) {
-		case 1:
-			Action action = new BasicAttack();
-	        //if (this.power > 50) action = new PowerfulAttack(action);
-	        if (this.power < 40) action = new LightningAttack(action);
-
-	        System.out.println(name + " usa: " + action.getDescription());
-	        Integer totalDamage = action.execute(this, opponent);
-	        System.out.println(opponent.getName() + " recibe " + totalDamage + " de daño. Vida restante: " + opponent.getHealth());
-		break;
-		case 2:
-			damage1=calculator.calculateDamage(this,opponent);
-			if(damage1==0) {
-				System.out.println(this.getName()+" se protege ante el ataque");
-			}else {
-				this.setHealth(opponent.health-damage1);
-				System.out.println(this.getName()+ "ataca y genera "+ damage1+ "de daño sobre "+ opponent.getName());
-			}
-		break;
-	}
-    	
-    	
-    	if (state != null) {
+		//Comprobamos el estado
+		if (state != null) {
             state.handle(this);
 
             // Si está paralizado, se podría interrumpir la acción:
             if (state instanceof ParalyzedState) {
                 System.out.println(name + " está paralizado y no puede actuar.");
                 return;
-            }
+            }else{
+				switch(playerAction) {
+				case 1:
+					Action action = new BasicAttack();
+					//if (this.power > 50) action = new PowerfulAttack(action);
+					if (this.power < 40) action = new LightningAttack(action);
+	
+					System.out.println(name + " usa: " + action.getDescription());
+					Integer totalDamage = action.execute(this, opponent);
+					System.out.println(opponent.getName() + " recibe " + totalDamage + " de daño. Vida restante: " + opponent.getHealth());
+				break;
+				case 2:
+					damage1=calculator.calculateDamage(this,opponent);
+					if(damage1==0) {
+						System.out.println(this.getName()+" se protege ante el ataque");
+					}else {
+						this.setHealth(opponent.health-damage1);
+						System.out.println(this.getName()+ " ataca y genera "+ damage1+ " de daño sobre "+ opponent.getName());
+					}
+				break;
+				}
+			}
         }
-
-        
+    	
     }
     
 }
