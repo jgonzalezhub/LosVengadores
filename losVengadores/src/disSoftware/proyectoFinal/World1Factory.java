79f55a0ef4cb84d5f78e709dd95bd718101742ef
Jaime744 <jaim3cr94@gmail.com>
2025-05-08 23:06:43 +0200

    update stats de enemigos y de player
    ya tienen defensa

--- losVengadores/src/disSoftware/proyectoFinal/World1Factory.java
+++ losVengadores/src/disSoftware/proyectoFinal/World1Factory.java
@@ -4,28 +4,28 @@ public class World1Factory implements EnemyFactory {
 
 	@Override
 	public Enemy createIronMan() {
-		Enemy ironMan = new IronMan("Iron-Man", 40, 100,0, "JARVIS CLIP THAT", 30, null);
+		Enemy ironMan = new IronMan("Iron-Man", 30, 100,15, "JARVIS CLIP THAT", 30, null);
 		ironMan.setStrategy(new IronManStrategy());
 		return ironMan;
 	}
 
 	@Override
 	public Enemy createSpiderMan() {
-		Enemy spiderMan = new SpiderMan("Spider-Man", 40, 80,0, "Don't mess with the amazing Spider-Man", 20, null);
+		Enemy spiderMan = new SpiderMan("Spider-Man", 30, 80,20, "Don't mess with the amazing Spider-Man", 20, null);
 		spiderMan.setStrategy(new SpidermanStrategy());
 		return spiderMan;
 	}
 
 	@Override
 	public Enemy createThor() {
-		Enemy thor = new Thor("Thor: God of Thunder", 30, 120,0, "Behold the God of Thunder", 40, null);
+		Enemy thor = new Thor("Thor: God of Thunder", 35, 120,15, "Behold the God of Thunder", 40, null);
 		thor.setStrategy(new ThorStrategy());
 		return thor;
 	}
 
 	@Override
 	public Enemy createCaptainAmerica() {
-		Enemy captainAmerica = new CaptainAmerica("Captain America", 40, 100,0, "AVENGERS ASSEMBLE", 20, null);
+		Enemy captainAmerica = new CaptainAmerica("Captain America", 35, 100,20, "AVENGERS ASSEMBLE", 20, null);
 		captainAmerica.setStrategy(new CaptainAmericaStrategy());
 		return captainAmerica;
 	}
--- losVengadores/src/disSoftware/proyectoFinal/World2Factory.java
+++ losVengadores/src/disSoftware/proyectoFinal/World2Factory.java
@@ -4,25 +4,25 @@ public class World2Factory implements EnemyFactory {
 
 	@Override
 	public Enemy createIronMan() {
-		Enemy ironMan = new IronMan("Iron-Man", 20, 100,0, "JARVIS CLIP THAT", 30, null);
+		Enemy ironMan = new IronMan("Iron-Man", 45, 200,15, "JARVIS CLIP THAT", 30, null);
 		return ironMan;
 	}
 
 	@Override
 	public Enemy createSpiderMan() {
-		Enemy spiderMan = new SpiderMan("Spider-Man", 40, 80,0, "Don't mess with the amazing Spider-Man", 20, null);
+		Enemy spiderMan = new SpiderMan("Spider-Man", 45, 160,15, "Don't mess with the amazing Spider-Man", 20, null);
 		return spiderMan;
 	}
 
 	@Override
 	public Enemy createThor() {
-		Enemy thor = new Thor("Thor: God of Thunder", 30, 120,0, "Behold the God of Thunder", 40, null);
+		Enemy thor = new Thor("Thor: God of Thunder", 60, 200,10, "Behold the God of Thunder", 40, null);
 		return thor;
 	}
 
 	@Override
 	public Enemy createCaptainAmerica() {
-		Enemy captainAmerica = new CaptainAmerica("Captain America", 40, 100,0, "AVENGERS ASSEMBLE", 20, null);
+		Enemy captainAmerica = new CaptainAmerica("Captain America", 65, 190,10, "AVENGERS ASSEMBLE", 20, null);
 		return captainAmerica;
 	}
 }
