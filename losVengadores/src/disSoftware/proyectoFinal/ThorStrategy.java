a55190e0afe2e277216561c0b6582a1ca84583cf
Jaime744 <jaim3cr94@gmail.com>
2025-05-08 23:53:57 +0200

    cambios en las estrategias

--- losVengadores/src/disSoftware/proyectoFinal/ThorStrategy.java
+++ losVengadores/src/disSoftware/proyectoFinal/ThorStrategy.java
@@ -7,11 +7,12 @@ public class ThorStrategy implements Strategy {
     	int damage = DamageCalculator.getInstance().calculateDamage(self, opponent);
     	System.out.println("Thor usa su martillo y genera " + self.getPower() + " de daño ");
     	if(self.getHealth()<=30) {
+    		System.out.println("Como esta herido recurre a su movimiento definitivo ");
 	        System.out.println(self.getName() + " ataca con rayos a " + opponent.getName());
 	        damage += self.getSpecialNumber();
 	        String ultimateText=self.getSpecialText();
 	        System.out.println(ultimateText );
-	        System.out.println(opponent.getName() + " recibe " + damage + " de daño extra.");
+	        System.out.println(opponent.getName() + " recibe " + damage);
     	}
 		opponent.takeDamage(damage);
 	        return damage;
